f0f9a707670eedf480ca4c964ba1c485ff8f5828
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2019-12-10 02:22:39 +0530

    Add files via upload

---
+++ WEBT/prog7.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="1" charset="UTF-8"/>
+        <style>
+            p{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+                font-size: 80px;
+            }
+            body{
+                background-color: black;
+                color: white;
+            }
+        </style>
+        <p>
+            <?php echo date("H:i:s A");?>
+        </p>
+    </head>
+</html>
