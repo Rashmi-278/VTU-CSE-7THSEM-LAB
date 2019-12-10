511b607b8b5ad1402394d074b53c931665a5abc2
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2019-12-10 18:16:41 +0530

    Update prog10.php

--- WEBT/prog10.php
+++ WEBT/prog10.php
@@ -1,96 +1,117 @@
 <!DOCTYPE html>
 <html>
-    <h3>SUNNY ARYA 1JS15CS103</h3>
+<head>
+	<title>1JS16CS081 PROG10</title>
+</head>
 <body>
-<style>
-    table,td,th
-    {
-    border:1px solid black;
-    width:33%;
-    text-align:center;
-    border-collapse:collapse;
-    background-color:lightblue;
-    }
-    table {margin:auto;}
+<style type="text/css">
+	table,th,tr{
+		border:2px solid black;
+		text-align:center;
+		width:33%;
+		border-collapse:collapse;
+		background-color:lightblue;
+	}
+	table{margin: auto;}
+
 </style>
-<?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="student";
+<?php 
+
 $a=array();
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+
+$conn = mysqli_connect("localhost","root","","student");
 if($conn->connect_error)
-    die("Connection failed:".$conn->connect_error);
-$sql="SELECT * FROM learner";
-$result=$conn->query($sql);
-echo"<br>";
-echo"<center>BEFORE SORTING</center>";
-echo"<table border='2'>";
-echo"<tr>";
-echo"<th>USN</th><th>NAME</th><th>Address</th></tr>";
+{
+	die("ERROR".$conn->connect_error."<br>");
+}
+
+$sql = "SELECT * FROM learner";
+
+$result = $conn->query($sql);
+
+echo "BEFORE SORTING <br>";
+echo "<table>";
+echo "<tr>";
+echo "<th>USN</th><th>NAME</th><th>ADDR</th>";
+echo "</tr>";
+
 if($result->num_rows>0)
 {
-    while($row=$result->fetch_assoc())
-    {
-    echo"<tr>";
-    echo"<td>".$row["usn"]."</td>";
-    echo"<td>".$row["name"]."</td>";
-    echo"<td>".$row["addr"]."</td></tr>";
-    array_push($a,$row["usn"]);
-    }
+	while($row = $result->fetch_assoc())
+	{
+		echo "<tr>";
+		echo "<td>".$row['usn']."</td>";
+		echo "<td>".$row['name']."</td>";
+		echo "<td>".$row['addr']."</td>";
+		echo "</tr>";
+		array_push($a,$row['usn']);
+	}
+
+
 }
 else
-    echo"Table is empty";
-echo"</table>";
-$n=count($a);
-$b=$a;
-for($i=0;$i<($n-1);$i++)
 {
-$pos=$i;
-    for($j=$i+1;$j<$n;$j++)
-    {
-        if($a[$pos]>$a[$j])
-        $pos=$j;
-    }
-    if($pos!=$i)
-    {
-        $temp=$a[$i];
-        $a[$i]=$a[$pos];
-        $a[$pos]=$temp;
-    }
+	echo "TABLE IS EMPTY";	
 }
-$c=array();
-$d=array();
-$result=$conn->query($sql);
-if($result->num_rows>0)
+
+echo "</table>";
+
+$n = count($a);
+$b = $a;
+
+for($i = 0;$i<($n-1);$i++)
 {
-    while($row=$result->fetch_assoc())
-    {
-      for($i=0;$i<$n;$i++)
-      {
-          if($row["usn"]==$a[$i])
-          {
-          $c[$i]=$row["name"];
-          $d[$i]=$row["addr"];
-          }
-      }
-    }
+	for($j=$i+1; $j<$n ; $j++)
+	{
+		if($a[$i]>$a[$j])
+		{
+			$temp = $a[$i];
+			$a[$i] = $a[$j];
+			$a[$j] = $temp;
+		}
+	}
+
 }
-echo"<br>";
-echo"<center>AFTER SORTING<center>";
-echo"<table border='2'>";
-echo"<tr>";
-echo"<th>USN</th><th>NAME</th><th>Address</th></tr>";
-for($i=0;$i<$n;$i++)
+
+$results = $conn->query($sql);
+
+
+if($results->num_rows>0)
 {
-    echo"<tr>";
-    echo"<td>".$a[$i]."</td>";
-    echo"<td>".$c[$i]."</td>";
-    echo"<td>".$d[$i]."</td>";
+	while($row = $results->fetch_assoc())
+	{
+		
+		for($i = 0;$i<$n;$i++)
+		{	
+			if($a[$i] == $row['usn'])
+			{
+				$c[$i] = $row['name'];
+				$d[$i] = $row['addr'];
+			}
+
+
+		}
+	}
+
+
 }
-echo"</table>";
-$conn->close();
-?>
+
+echo "BEFORE SORTING <br>";
+echo "<table>";
+echo "<tr>";
+echo "<th>USN</th><th>NAME</th><th>ADDR</th>";
+echo "</tr>";
+for($i = 0;$i<$n;$i++)
+{
+	echo "<tr>";
+		echo "<td>".$a[$i]."</td>";
+		echo "<td>".$c[$i]."</td>";
+		echo "<td>".$d[$i]."</td>";
+		echo "</tr>";
+}
+
+echo "</table>" ; 
+
+ ?>
 </body>
 </html>
